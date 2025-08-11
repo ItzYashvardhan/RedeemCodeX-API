10d13341b0a9e803567ef231b05f33301e5562a0
Yashvardhan76 <itsyashvardhan76@gmail.com>
2025-08-11 09:57:23 +0530

    updated to 1.1.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ plugins {
 }
 
 group = "com.github.ItzYashvardhan"
-version = "1.1.3"
+version = "1.1.4"
 repositories {
     mavenCentral()
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") {
