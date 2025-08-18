73f9a4387495a295ef459fadd094de88e0954356
Yashvardhan76 <itsyashvardhan76@gmail.com>
2025-08-18 16:56:40 +0530

    also this too

--- src/main/kotlin/api/justlime/redeemcodex/models/RedeemLog.kt
+++ src/main/kotlin/api/justlime/redeemcodex/models/RedeemLog.kt
@@ -1,10 +1,11 @@
 package api.justlime.redeemcodex.models
 
+import java.sql.Timestamp
 import java.util.UUID
 
 data class RedeemLog (
     val id: Int,
     val playerUUID: UUID,
     val code: String,
-    val redeemedAt: Long
+    val redeemedAt: Timestamp
 )
