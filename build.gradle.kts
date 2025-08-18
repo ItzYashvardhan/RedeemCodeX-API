54972da856a0b18cac6cfeaf90cee86c3a4919a5
Yashvardhan76 <itsyashvardhan76@gmail.com>
2025-08-19 02:10:56 +0530

    Releasing 1.2.0...

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ plugins {
 }
 
 group = "com.github.ItzYashvardhan"
-version = "1.1.4"
+version = "1.2.0"
 repositories {
     mavenCentral()
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") {
